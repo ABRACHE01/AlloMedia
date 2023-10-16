c59e32a60487253bcf397e2303c3be1e93dae9c3
abrache <abrache2001@gmail.com>
2023-10-16 10:31:18 +0100

    implementing the feature register

---
+++ backend/models/userModel.js
@@ -0,0 +1,30 @@
+import mongoose from "mongoose"
+
+
+const userSchema = new mongoose.Schema({
+    name:{
+        type :String ,
+        required : [true , 'Please add a name' ]
+    },
+    email:{
+        type : String ,
+        required : [true , 'Please add an email ' ],
+        unique: true 
+    },
+    password:{
+        type : String ,
+        required : [true , 'Please add a password ' ]
+    },
+    role: {
+        type: String,
+        required: [true, 'Please add your role'],
+        enum: ['client', 'deliver'],
+    }
+},{
+    timestamps : true 
+})
+
+const User = mongoose.model('User', userSchema);
+
+
+export { User }

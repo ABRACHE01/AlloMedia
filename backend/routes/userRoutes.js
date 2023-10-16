b246d3b14717851db065c54ca54f72e08ccc81dc
abrache <abrache2001@gmail.com>
2023-10-16 11:17:10 +0100

    AM-1 immementing the feature login

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -3,8 +3,10 @@ const userRoutes = express.Router();
 import {userController} from "../controllers/userController.js"
 
 const UserController = new userController() ; 
-
+//register
 userRoutes.post('/', UserController.registerUser)
+
+//login 
 userRoutes.post('/login', UserController.loginUser)
 
 

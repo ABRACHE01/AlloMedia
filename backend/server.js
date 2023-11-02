ca2515689eb8e2bc1fa5e89eeb591cb856970652
abrache <abrache2001@gmail.com>
2023-11-02 09:12:32 +0100

    AM-10 adding swagger backend routing documentation

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,5 @@
 
 import  express from "express";
-import colors from "colors"
 import {connectDB} from "./config/database.js"
 import {seedRoles} from "./config/roleSeeder.js"
 import cookieParser from "cookie-parser"
@@ -9,6 +8,8 @@ import {userRoutes} from './routes/userRoutes.js'
 import {adminRoutes} from './routes/adminRoutes.js'
 import {clientRoutes} from './routes/clientRoutes.js'
 import {deliverRoutes} from './routes/deliverRoutes.js'
+import swaggerJsdoc from "swagger-jsdoc"
+import swaggerUi from "swagger-ui-express"
 
 connectDB();
 
@@ -16,7 +17,7 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 
-const allowedOrigins = ['http://localhost:5173']; 
+const allowedOrigins = ['http://localhost:5173' , 'http://localhost:3000' ]; 
 
 const corsOptions = {
   origin: function (origin, callback) {
@@ -45,6 +46,41 @@ app.use("/api/auth", userRoutes)
 app.use("/api/user", adminRoutes)
 app.use("/api/user", clientRoutes)
 app.use("/api/user", deliverRoutes)
+
+
+const options = {
+  definition: {
+    openapi: "3.1.0",
+    info: {
+      title: "LogRocket Express API with Swagger",
+      version: "0.1.0",
+      description:
+        "This is a simple CRUD API application made with Express and documented with Swagger",
+      license: {
+        name: "MIT",
+        url: "https://spdx.org/licenses/MIT.html",
+      },
+      contact: {
+        name: "LogRocket",
+        url: "https://logrocket.com",
+        email: "info@email.com",
+      },
+    },
+    servers: [
+      {
+        url: "http://localhost:3000",
+      },
+    ],
+  },
+  apis: ["./routes/*.js"],
+};
+
+const specs = swaggerJsdoc(options);
+app.use(
+  "/api-docs",
+  swaggerUi.serve,
+  swaggerUi.setup(specs, { explorer: true })
+);
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);

b6b37e4bf31dee4dac80f2162a58ea40b882721b
abrache <abrache2001@gmail.com>
2023-11-02 12:19:01 +0100

    AM-10 adding swagger backend routing documentation

--- backend/routes/adminRoutes.js
+++ backend/routes/adminRoutes.js
@@ -6,11 +6,69 @@ import { adminController } from '../controllers/adminController.js';
 
 
 
+/**
+ * @swagger
+ * api/user/admin/me:
+ *   get:
+ *     summary: Get Admin Profile
+ *     description: Retrieves the profile information of the authenticated admin user.
+ *     security:
+ *       - bearerAuth: []
+ *     tags: [Admin]
+ *     responses:
+ *       '200':
+ *         description: Successful operation
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/AdminProfile'
+ */
 adminRoutes.get('/admin/me',authMiddleware.protect ,adminController.adminProfile)
 
+/**
+ * @swagger
+ * api/user/admin/profileResetPass:
+ *   post:
+ *     summary: Send Reset Password Email
+ *     description: Sends a reset password email to the authenticated admin user.
+ *     security:
+ *       - bearerAuth: []
+ *     tags: [Admin]
+ *     responses:
+ *       '200':
+ *         description: Email sent successfully
+ */
 //send email after auth for rest
 adminRoutes.post("/admin/profileResetPass", authMiddleware.protect, adminController.sendEmail);
 
+
+/**
+ * @swagger
+ * api/user/admin/newPassloggedin/{token}:
+ *   post:
+ *     summary: Reset Password
+ *     description: Resets the password for the authenticated admin user.
+ *     security:
+ *       - bearerAuth: []
+ *     tags: [Admin]
+ *     parameters:
+ *       - in: path
+ *         name: token
+ *         required: true
+ *         schema:
+ *           type: string
+ *         description: Reset password token received via email
+ *     requestBody:
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/ResetPasswordPayload'
+ *     responses:
+ *       '200':
+ *         description: Password reset successful
+ *       '400':
+ *         description: Invalid token
+ */
 //reset password after auth 
 adminRoutes.post("/admin/newPassloggedin/:token",authMiddleware.protect, adminController.resetPasswordAsLoggedIn);
 
--- backend/routes/deliverRoutes.js
+++ backend/routes/deliverRoutes.js
@@ -4,14 +4,69 @@ import {authMiddleware} from "../middlewares/authMiddleware.js"
 import { deliverController } from '../controllers/deliverController.js';
 
 
-
+/**
+ * @swagger
+ * /api/user/deliver/me:
+ *   get:
+ *     summary: Get Deliverer Profile
+ *     description: Retrieves the profile information of the authenticated deliverer user.
+ *     security:
+ *       - bearerAuth: []
+ *     tags: [Deliverer]
+ *     responses:
+ *       '200':
+ *         description: Successful operation
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/DelivererProfile'
+ */
 deliverRoutes.get('/deliver/me',authMiddleware.protect ,deliverController.deliverProfile)
 
-
+/**
+ * @swagger
+ * /api/user/deliver/profileResetPass:
+ *   post:
+ *     summary: Send Reset Password Email
+ *     description: Sends a reset password email to the authenticated deliverer user.
+ *     security:
+ *       - bearerAuth: []
+ *     tags: [Deliverer]
+ *     responses:
+ *       '200':
+ *         description: Email sent successfully
+ */
 //send email after auth for rest
 deliverRoutes.post("/deliver/profileResetPass", authMiddleware.protect, deliverController.sendEmail);
 
 //reset password after auth 
+/**
+ * @swagger
+ * /api/user/deliver/newPassloggedin/{token}:
+ *   post:
+ *     summary: Reset Password
+ *     description: Resets the password for the authenticated deliverer user.
+ *     security:
+ *       - bearerAuth: []
+ *     tags: [Deliverer]
+ *     parameters:
+ *       - in: path
+ *         name: token
+ *         required: true
+ *         schema:
+ *           type: string
+ *         description: Reset password token received via email
+ *     requestBody:
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/ResetPasswordPayload'
+ *     responses:
+ *       '200':
+ *         description: Password reset successful
+ *       '400':
+ *         description: Invalid token
+ */
 deliverRoutes.post("/deliver/newPassloggedin/:token",authMiddleware.protect, deliverController.resetPasswordAsLoggedIn);
 
 export {deliverRoutes}
